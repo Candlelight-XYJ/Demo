9ea8f200d47c6c5720b957f002f331ac48401116
xiangyujia <xiangyj_hzau@163.com>
2019-04-27 19:12:49 +0800

    update ui

--- IDmapApp/utilities.R
+++ IDmapApp/utilities.R
@@ -7,3 +7,30 @@ for(i in 1:length(allfiles)){
 }
 
 ## covert .Rdata to .csv
+datasets <- ls()
+datasets <- datasets[c(-13,-14)] ## delete the element in vector
+datasets
+
+## 
+ proteinDat <- lncRNA_human_protein
+ noncDat <- lncRNA_human_nonc
+ datNames <- names(proteinDat)
+ allRes <- data.frame()
+
+ for(j in 1:length(datNames)){
+ print(j)      
+ ## protein
+ pAnno <- eval(parse(text = paste0("proteinDat","$",datNames[j])))
+ pAnno$gpl <- rep(datNames[j],times=nrow(pAnno))
+ pAnno$biotype <- rep("protein",times=nrow(pAnno))
+ ## non-coding
+ nAnno <- eval(parse(text = paste0("noncDat","$",datNames[j])))
+ nAnno$gpl <- rep(datNames[j],times=nrow(nAnno))
+ nAnno$biotype <- rep("nonc",times=nrow(nAnno))
+ ## combine protein & non-coding
+ combineRes <- rbind(pAnno,nAnno)
+ allRes <- rbind(allRes,combineRes)
+ }
+write.csv(allRes,"E:\\学习资料存放处\\IDmap\\mySQL_table\\lncRNA_human_AllData.csv",row.names=F)
+
+
