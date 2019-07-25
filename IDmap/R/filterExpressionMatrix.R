d56a1d18207621aa387026a99c01d81d123d71b6
xiangyujia <xiangyj_hzau@163.com>
2019-07-25 21:03:40 +0800

    update filterEM function

--- IDmap/R/filterExpressionMatrix.R
+++ IDmap/R/filterExpressionMatrix.R
@@ -1,31 +1,43 @@
 ########################################
 ##                                    ##
-## code last update: July 12, 2019    ##
+## code last update: July 25, 2019    ##
 ##                                    ##
 ########################################
 
 ##' Filter expression matrix
 ##'
 ##' \code{filterEM} returns
-##' @param dat dat is a expression matrix,which the first col must be probe ids and the second col must be gene symbols or entrez ids. Other cols should be expression values .
-##'
+##' @param matrix is an expression matrix which rownames are probe ids and every column is a sample
+##' @param probe2gene is a data frame which includes probe ids and corresponding gene Symbols
+##' @param omitNA omit NA symbol rows in matrix
 ##' @return filter ids expression ma
 ##'
 ##' @examples
-##' probeid <- c("A_23_P101521","A_33_P3695548","A_33_P3266889")
-##' symbol <- c("symbol1","symbol2","s)
-##' dat <- data.frame
+##' eMatrix <- system.file("extdata", "matrixData.Rdata", package="IDmap")
+##' allMapIds <- system.file("extdata", "probe2gene.Rdata", package="IDmap")
+##' load(eMatrix)
+##' load(allMapIds)
+##' ## head(matrixData)
+##' ## head(probe2gene)
+##' res <- filterEM(matrixData,probe2gene,omitNA=T)
 ##' @export
-filterEM <- function(dat){
+filterEM <- function(matrix,probe2gene,omitNA=T){
+  ids <- probe2gene
+  colnames(ids) <- c("probeid","symbol")
   # computing median values of expression matrix
-  dat$EMmedian <- apply(dat,1,median)
-  # select probe ids, gene symbols and median values from dat
-  ids <- with(dat,probeid=dat[,1],symbol=dat[,2],median=EMmedian)
+  ids$EMmedian <- apply(matrix,1,median)
   # ordering matrix by median values
-  ids=ids[order(ids$median,decreasing = T),]
+  ids=ids[order(ids$EMmedian,decreasing = T),]
   # filter duplicated symbols
-  ids <- ids[!duplicated(ids$symbol),]
-  res <- dat[which(dat[,1] %in% ids$probeid),]
+  ids <- ids[!duplicated(ids[,2]),]
+  #
+  res <- matrix[which(rownames(matrix) %in% ids[,1]),]
+  res <- as.data.frame(res)
+  res$probeid <- rownames(res)
+  res <- merge(res,ids,by="probeid",all=F)
+  if(omitNA){
+  res <- na.omit(res)
+  }
   return(res)
 }
 
