481fac83b110ac70f04db0b0febf213318c76e7d
xiangyujia <xiangyj_hzau@163.com>
2019-08-21 20:05:38 +0800

    update functions

--- IDmap/R/convertID.R
+++ IDmap/R/convertID.R
@@ -7,7 +7,7 @@
 ##' Converting biological ID types
 ##'
 ##' \code{convertID} convert
-##' @param geneID GPL(GEO platform) number, eg: GPL570
+##' @param geneID gene symbol
 ##' @param fromType of probe anntation stored, one of "pipe", "bioc", "soft", "all"
 ##' @param toType GENCODE biotypes,eg:"protein_coding","non_coding","pseudogene" ...
 ##' @return data.frame
--- IDmap/R/saveRdata.R
+++ IDmap/R/saveRdata.R
@@ -7,23 +7,14 @@
 #' @keywords refresh Rdata
 
 refreshRdata <- function(refresh = F) {
+  #setwd("E:/GitHub/Demo/IDmap/")
   if (refresh) {
     allfiles = list.files(getwd())
     for(i in 1:length(allfiles)){
       print(i)
       eval(parse(text = paste0("load(","\'",allfiles[i],"\'",")")))
     }
-    human_all_anno <- human_all_anno2
-    mouse_all_anno <- mouse_all_anno2
-    rat_all_anno <- rat_all_anno2
-    human_lncRNA_anno <- human_lncRNA_anno2
-    mouse_lncRNA_anno <- mouse_lncRNA_anno2
-    rat_lncRNA_anno <- rat_lncRNA_anno2
-    #load('../Annotations/human_all_anno.Rdata')
-    #colnames(human_all_anno)
-    #head(human_all_anno)
 
-    #setwd("E:/学习资料存放处/IDmap/IDmap_edit/IDmap/")
     devtools::use_data(human_all_anno, overwrite = T)
     devtools::use_data(mouse_all_anno, overwrite = T)
     devtools::use_data(rat_all_anno, overwrite = T)
